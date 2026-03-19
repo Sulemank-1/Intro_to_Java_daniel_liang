01f753b164c49a930221395e5f8cb08816b9dc1f
Sulemank-1 <suleman.dev42@gmail.com>
2026-03-19 22:02:24 +0500

    Study Section 11.8: Dynamic Binding

---
+++ src/A11_Inheritance_And_Polymorphism/DynamicBindingDemo.java
@@ -0,0 +1,34 @@
+package A11_Inheritance_And_Polymorphism;
+
+public class DynamicBindingDemo {
+    static void main() {
+        m(new GraduateStudent());
+        m(new Student());
+        m(new Personn());
+        m(new Object());
+    }
+
+    public static void m(Object x){
+        System.out.println(x.toString());
+    }
+
+
+}
+
+class GraduateStudent extends Student{
+
+}
+
+class Student extends Personn{
+    @Override
+    public String toString(){
+        return "Student";
+    }
+}
+
+class Personn extends Object{
+    @Override
+    public String toString(){
+        return "Person";
+    }
+}
