75ff0054f28959eb97d5d1f0dae57104d6dd7170
Sulemank-1 <suleman.dev42@gmail.com>
2026-03-20 01:29:28 +0500

    Study Section 11.9: Casting Objects and the instanceof Operator

---
+++ src/A11_Inheritance_And_Polymorphism/CastingDemo.java
@@ -0,0 +1,20 @@
+package A11_Inheritance_And_Polymorphism;
+
+public class CastingDemo {
+    public static void main(String[] args) {
+        Object object1 = new Circle(1);
+        Object object2 = new Rectangle(1,1);
+
+        displayObject(object1);
+        displayObject(object2);
+    }
+
+    public static void displayObject(Object object){
+        if (object instanceof Circle){
+            System.out.println("The circle area is " + ((Circle)object).getArea());
+            System.out.println("The circle diameter is " + ((Circle)object).getDiameter());
+        } else if (object instanceof Rectangle) {
+            System.out.println("The circle diameter is " + ((Rectangle)object).getArea());
+        }
+    }
+}
