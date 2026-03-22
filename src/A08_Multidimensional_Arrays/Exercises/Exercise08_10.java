3dff4287c5de54f960279191ffa82af73bf7bd80
Sulemank-1 <suleman.dev42@gmail.com>
2026-03-23 03:26:21 +0500

    Complete Ex 8.10

---
+++ src/A08_Multidimensional_Arrays/Exercises/Exercise08_10.java
@@ -0,0 +1,59 @@
+package A08_Multidimensional_Arrays.Exercises;
+
+public class Exercise08_10 {
+    public static void main(String[] args) {
+        int n = 4;
+
+        int[][] matrix = new int[n][n];
+
+        fillMatrix(matrix);
+        printMatrix(matrix);
+        findLargestRowAndColumn(matrix);
+    }
+
+    public static void fillMatrix(int[][] matrix){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = (int)(Math.random() * 2);
+            }
+        }
+    }
+
+    public static void printMatrix(int[][] matrix){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void findLargestRowAndColumn(int[][] matrix){
+        int[] rCount = new int[matrix.length];
+        int[] cCount = new int[matrix[0].length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 1){
+                    rCount[i]++;
+                    cCount[j]++;
+                }
+            }
+        }
+
+        System.out.println("row index " + findMax(rCount));
+        System.out.println("column index " + findMax(cCount));
+    }
+
+    public static int findMax(int[] array){
+        int max = array[0];
+        int maxIndex = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+}
