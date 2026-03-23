989d01c7bef2c6acb09feef0cff6051c7b18b7f7
Sulemank-1 <suleman.dev42@gmail.com>
2026-03-24 02:18:21 +0500

    Complete Ex 11.13

---
+++ src/A11_Inheritance_And_Polymorphism/Exercises/Exercise11_13.java
@@ -0,0 +1,35 @@
+package A11_Inheritance_And_Polymorphism.Exercises;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Exercise11_13 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        System.out.print("Enter ten numbers: ");
+        for (int i = 0; i < 10; i++) {
+            numbers.add(input.nextInt());
+        }
+
+        removeDuplicates(numbers);
+
+        for (int i : numbers) {
+            System.out.print(i + " ");
+        }
+    }
+
+
+    public static void removeDuplicates(ArrayList<Integer> list) {
+
+         for (int i = 0; i < list.size(); i++) {
+            int num = list.get(i);
+            for (int j = list.size() - 1; j > i; j--)
+                if (num == list.get(j))
+                    list.remove(j);
+        }
+    }
+}
+
