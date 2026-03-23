b85eb546fdb3a2b3f6c25a6fddd37966b8ba23fb
Sulemank-1 <suleman.dev42@gmail.com>
2026-03-23 19:56:45 +0500

    Complete Ex 11.10

---
+++ src/A11_Inheritance_And_Polymorphism/Exercises/Exercise11_10.java
@@ -0,0 +1,51 @@
+package A11_Inheritance_And_Polymorphism.Exercises;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Exercise11_10 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        MyStack stack = new MyStack();
+
+        System.out.print("Enter five strings: ");
+        for (int i = 0; i < 5; i++) {
+            stack.push(input.next());
+        }
+
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
+        }
+    }
+}
+
+
+class MyStack extends ArrayList<Object> {
+
+
+    public boolean isEmpty(){
+        return super.isEmpty();
+    }
+
+    public int getSize(){
+        return size();
+    }
+
+    public Object peek(){
+        return get(getSize() - 1);
+    }
+
+    public Object pop(){
+        return remove(getSize() - 1);
+    }
+
+    public void push(Object o){
+        add(o);
+    }
+
+    @Override
+    public String toString(){
+        return "Stack: " + super.toString();
+    }
+}
