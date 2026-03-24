94cef77701fa0226f0006c9c82d33140b8a92919
Sulemank-1 <suleman.dev42@gmail.com>
2026-03-24 21:17:07 +0500

    Complete Ex 11.16

---
+++ src/A11_Inheritance_And_Polymorphism/Exercises/Exercise11_16.java
@@ -0,0 +1,30 @@
+package A11_Inheritance_And_Polymorphism.Exercises;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Exercise11_16 {
+    public static void main(String[] args) {
+        int number1 = (int)(Math.random() * 10);
+        int number2 = (int)(Math.random() * 10);
+
+        ArrayList<Integer> answers = new ArrayList<>();
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is " + number1 + " + " + number2 + "? ");
+        int answer = input.nextInt();
+
+        while (number1 + number2 != answer){
+            if (answers.contains(answer))
+                System.out.println("You already entered" + answer);
+            else{
+                System.out.print("Wrong answer. Try again. What is " + number1 + " + " + number2 + "? ");
+                answers.add(answer);
+            }
+            answer = input.nextInt();
+        }
+        System.out.println("You got it!");
+    }
+
+}
