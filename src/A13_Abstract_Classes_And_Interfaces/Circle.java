2e72624f5081f3d71c0884eb786d9cfadabcb471
Sulemank-1 <suleman.dev42@gmail.com>
2026-04-13 01:41:10 +0500

    Study Section 13.2: Abstract Classes

---
+++ src/A13_Abstract_Classes_And_Interfaces/Circle.java
@@ -0,0 +1,54 @@
+package A13_Abstract_Classes_And_Interfaces;
+
+
+public class Circle extends GeometricObject {
+    private double radius;
+
+    public Circle(){}
+
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled){
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea(){
+        return radius * radius * Math.PI;
+    }
+
+    public double getDiameter(){
+        return 2 * radius;
+    }
+
+    public double getPerimeter(){
+        return 2 * radius * Math.PI;
+    }
+
+    public void printCircle(){
+        System.out.println("The circle is created " + super.getDateCreated() + " and the radius is " + radius);
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + "\nradius is " + radius;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (o instanceof Circle)
+            return radius == ((Circle)o).radius;
+        else
+            return false;
+    }
+}
