01895082f6000bf6997ae78ea817788088a99c1e
Sulemank-1 <suleman.dev42@gmail.com>
2026-04-24 21:59:24 +0500

    Complete Ex 13.2

---
+++ src/A13_Abstract_Classes_And_Interfaces/Exercises/Exercise13_02.java
@@ -0,0 +1,14 @@
+package A13_Abstract_Classes_And_Interfaces.Exercises;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Exercise13_02 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void shuffle(ArrayList<Number> list){
+        Collections.shuffle(list);
+    }
+}
