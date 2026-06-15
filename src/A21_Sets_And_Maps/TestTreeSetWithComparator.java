c7fe4672ee08d7de18a5bd4c448b469380bf8180
Sulemank-1 <suleman.dev42@gmail.com>
2026-06-16 01:32:24 +0500

    Study Section 21.2.3: TreeSet

---
+++ src/A21_Sets_And_Maps/TestTreeSetWithComparator.java
@@ -0,0 +1,20 @@
+package A21_Sets_And_Maps;
+
+import A13_Abstract_Classes_And_Interfaces.*;
+import A20_Lists_Stacks_Queues_and_Priority_Queues.GeometricObjectComparator;
+
+import java.util.*;
+
+public class TestTreeSetWithComparator {
+    public static void main(String[] args) {
+        Set<GeometricObject> set = new TreeSet<>(new GeometricObjectComparator());
+        set.add(new Rectangle(4, 5));
+        set.add(new Circle(40));
+        set.add(new Circle(40));
+        set.add(new Rectangle(4, 1));
+
+        System.out.println("A sorted set of geometric objects");
+        for (GeometricObject element: set)
+            System.out.println("area = " + element.getArea());
+    }
+}
